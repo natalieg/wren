ca12c64a7ce70a990844cd1f5ad15e0198d3de68
Natalie <natalie.gornicki@gmail.com>
2026-08-16 23:50:05 +0200

    Mini Styling Fix

--- src/components/elements/EmojiMiniCard.jsx
+++ src/components/elements/EmojiMiniCard.jsx
@@ -1,12 +1,12 @@
-export default function EmojiMiniCard({ id, title, subtitle, emoji, onClick, active = false, otherActive, activeBg = 'bg-gradient-success', success }) {
+export default function EmojiMiniCard({ id, title, subtitle, emoji, onClick, active = false, otherActive, success }) {
 
    const borderColor = active && success ? 'border-success/50' : (otherActive ? 'border-white' : 'border-yellow-600/50')
    const bgColor = active && success ? 'bg-gradient-success' : (active && !success ? 'bg-gradient-main' : 'bg-bg-base gradientHover')
 
    return (
       <div id={id} onClick={onClick}
-         className={`flex flex-col items-center justify-center gap-1 rounded-md p-2 cursor-pointer border ${borderColor} 
-            ${active ? activeBg : bgColor}`}>
+         className={`flex flex-col items-center justify-center gap-1 rounded-md p-2 cursor-pointer border ${borderColor}
+            ${bgColor}`}>
          <p className='text-shadow-md text-shadow-black/30 text-4xl'>{emoji}</p>
          <p className='font-retro'>{title}</p>
          <p className='font-retro'>{subtitle}</p>
