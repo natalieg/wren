de895a9ec15f4691b127ce464dc3bd77d9da2afd
Natalie <natalie.gornicki@gmail.com>
2026-08-18 19:02:30 +0200

    bar fix

--- src/components/elements/VerticalBar.jsx
+++ src/components/elements/VerticalBar.jsx
@@ -8,10 +8,10 @@ export default function VerticalBar({ value1, value2, hover1, hover2, maxValue1,
          <div className='h-32 w-full flex flex-col-reverse items-center'>
             <div className={`${!value2 && 'rounded-t-sm'} w-4/5 rounded-b-sm bg-gradient-success`}
                title={hover1 || value1}
-               style={{ height: `${bottomBar}%` }} />
+               style={{ height: `${bottomBar}px` }} />
             <div className={`${!value1 && 'rounded-b-sm'} w-4/5 rounded-t-sm bg-gradient-main`}
                title={hover2 || value2}
-               style={{ height: `${topBar}%` }} />
+               style={{ height: `${topBar}px` }} />
          </div>
          {label && <span
             className={`text-xs b-2 px-1 rounded-xs ${markSpecial ? 'bg-gradient-success' : 'text-text-secondary'}`}>
